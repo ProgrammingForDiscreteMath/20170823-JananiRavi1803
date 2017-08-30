74fbaa9b9aff1428be6ad4f9610357f0c77c38d9
Janani Ravichandran <31205235+JananiRavi1803@users.noreply.github.com>
2017-08-30 11:02:18 +0000

    Solved

--- code.py
+++ code.py
@@ -18,7 +18,8 @@ def entries_less_than_ten(L):
     Returns:
         A sublist of L consisting of those entries which are less than 10.
     """
-    return #Add your code here
+    M = [i for i in L if i<10]
+    return M
 
 #Test
 #print entries_less_than_ten([2, 13, 4, 66, -5]) == [2, 4, 6, -5]
@@ -35,7 +36,10 @@ def number_of_negatives(L):
     Returns:
         number of entries of L which are negative
     """
-    pass ##YOUR CODE REPLACES THIS
+    M = [i for i in L if i<0]
+    l = len(M)
+    return l
+
 
 # TEST
 #print number_of_negatives([2, -1, 3, 0, -1, 0, -45, 21]) == 3
@@ -53,7 +57,8 @@ def common_elements(L1, L2):
         A list whose elements are the common elements of ``L1`` and
         ``L2`` WITHOUT DUPLICATES.
     """
-    pass # your code goes here
+    C= [i for i in L1 if i in L2]
+    return C
 
 #TEST
 #common_elements([1, 2, 1, 4, "bio", 6, 1], [4, 4, 2, 1, 3, 5]) == [1, 2, 4]
@@ -66,10 +71,13 @@ def fibonacci_generator():
     The Fibonacci sequence 1, 1, 2, 3, 5, 8, 13, 21,...
     is defined by a1=1, a2=1, and an = a(n-1) + a(n-2).
     """
-    pass # Hint: use the ``yield`` command.
+    a1, a2 = 1, 1
+    while True:
+        yield a1
+        a1, a2 = a2, a1+a2
 
 #TEST
-#f = fibonacci()
+#f = fibonacci_generator()
 #[f.next() for f in range(10)] == [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
 #5
@@ -77,7 +85,15 @@ def largest_fibonacci_before(n):
     """
     Return the largest Fibonacci number less than ``n``.
     """
-    pass #Your code goes here.
+    f = fibonacci_generator()
+    l = []
+    while True:
+        x= f.next()
+        l.append(x)
+        #print x
+        if x >= n:
+            break
+    return l[-2]
 
 #TEST
 #largest-fibonacci_before(55) == 34
@@ -89,7 +105,11 @@ def catalan_generator():
 
     For the definition of the Catalan number sequence see `OEIS <https://www.oeis.org/A000108>`.
     """
-    pass #Your code goes here.
+    a1, a2 = 0,1
+    while True:
+        yield a2
+        a1 = a1+1 
+        a2 = a2 * 2 * (2*a1-1) / (a1+1) 
 
 #TEST
 #c = catalan_generator()
@@ -101,3 +121,20 @@ def catalan_generator():
 ### CREATE YOUR OWN FUNCTION. Make sure it has a nice docstring.
 # See https://www.python.org/dev/peps/pep-0257/
 # for basic tips on docstrings.
+
+def factorial(n):
+    """
+    The function returns the factorial value of n
+    """
+    if n==0:
+        return 1
+    elif n>=1:
+        fact = 1
+        for i in range(1, n+1):
+            fact = fact*i
+        return fact
+
+#factorial(5)
+#120
+#factorial(10)
+#3628800
